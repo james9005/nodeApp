3971d2e67dea2d84e4c9c72bbadbcccabe9a616e
James David Todd <james9005@hotmail.co.uk>
2015-11-30 15:26:58 +0000

    added stuff which works

---
+++ twittertest.js
@@ -0,0 +1,10 @@
+/**
+ * Created by james on 30/11/2015.
+ */
+var Twitter = require('twitter');
+var client = new Twitter({
+    consumer_key: 'RM7bAiVnRLk8KtDqfdTOXnONW',
+    consumer_secret: '4PK9XPEfuW66DapU9U3HEJ7roULlOdV8mVZ90TfQu0pJVIedxz',
+    access_token_key: 'JamesTodd_',
+    access_token_secret: '176380223'
+});
