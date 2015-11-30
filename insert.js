caa20e187b38f9fa47fb3cb1253f39bcefdca677
James David Todd <james9005@hotmail.co.uk>
2015-11-30 14:54:22 +0000

    added stuff which works

---
+++ insert.js
@@ -0,0 +1,54 @@
+/**
+ * Created by james on 30/11/2015.
+ */
+/**
+ * Created by james on 30/11/2015.
+ */
+var mongodb = require('mongodb');
+//and our HTTP server
+var http = require('http');
+//setup our port
+var port = process.env.PORT || 1337;
+var url='mongodb://james:playstation3@ds054288.mongolab.com:54288/1201173';
+var MongoClient = mongodb.MongoClient;
+
+http.createServer(function(request, response) {
+    response.writeHead(200, { 'Content-Type': 'text/plain' });
+    response.write('Connecting \n');
+    // Use connect method to connect to the Server
+    MongoClient.connect(url, function (err, db) {
+        response.write('Connection Made \n');
+
+        if (err) {
+            response.write('Unable to connect to the mongoDB server. Error:' + err + "\n");
+            //Error so close connection
+            db.close();
+        } else {
+            //HURRAY!! We are connected. :)
+            response.write('Connection established to' + url +"\n");
+            var collection = db.collection('users');
+            var user1 = {name: 'modulus admin', age: 42, roles: ['admin', 'moderator', 'user']};
+            var user2 = {name: 'modulus user', age: 22, roles: ['user']};
+            var user3 = {name: 'modulus super admin', age: 92, roles: ['super-admin', 'admin', 'moderator', 'user']};
+
+
+            //Done Close connection
+            collection.insert([user1, user2, user3], function (err, result) {
+                if (err) {
+                    response.write('Insert failed ' + err + "\n");
+                } else {
+                    console.log(result);
+                    response.write('Inserted ' + result.insertedCount +' documents ok. +"\n"');
+                }
+                //Close connection
+                db.close();
+                response.end('Finished, Connection closed \n');
+                //remove any other db.close or response.end statement below this line
+            });
+
+        }
+
+    });
+
+}).listen(port);
+
