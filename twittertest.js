57a5c245e1c8de7e4db71fd85eba43fb389ffde0
James David Todd <james9005@hotmail.co.uk>
2015-11-30 15:29:49 +0000

    adding the node js twitter stuff

--- twittertest.js
+++ twittertest.js
@@ -5,6 +5,18 @@ var Twitter = require('twitter');
 var client = new Twitter({
     consumer_key: 'RM7bAiVnRLk8KtDqfdTOXnONW',
     consumer_secret: '4PK9XPEfuW66DapU9U3HEJ7roULlOdV8mVZ90TfQu0pJVIedxz',
-    access_token_key: 'JamesTodd_',
-    access_token_secret: '176380223'
+    access_token_key: '176380223-ldL5pUpSaWXI4SnIS46BvgWchjRCq9d4AmSHzp13',
+    access_token_secret: 'moo9D2ilFYbaTJy3ZDHedsKpFjnhES0NmJabw4QQ1kCsc'
 });
+
+http.createServer(function(request, response) {
+    response.writeHead(200, { 'Content-Type': 'text/plain' });
+
+    client.get('search/tweets', {q: 'lolcats'}, function(error, tweets){
+        console.log(tweets);
+    });
+
+}).listen(port);
+
+
+
