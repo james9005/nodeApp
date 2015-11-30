368da3a1fbf7098f28d725d521df722e4078a341
James David Todd <james9005@hotmail.co.uk>
2015-11-30 14:43:19 +0000

    added stuff which works

--- connect.js
+++ connect.js
@@ -6,5 +6,29 @@ var mongodb = require('mongodb');
 var http = require('http');
 //setup our port
 var port = process.env.PORT || 1337;
-var url='mongodb://<dbuser>:<dbpassword>@ds054288.mongolab.com:54288/1201173';
+var url='mongodb://<James>:<playstation3>@ds054288.mongolab.com:54288/1201173';
 var MongoClient = mongodb.MongoClient;
+http.createServer(function(request, response) {
+    response.writeHead(200, { 'Content-Type': 'text/plain' });
+    response.write('Connecting \n');
+    // Use connect method to connect to the Server
+    MongoClient.connect(url, function (err, db) {
+        response.write('Connection Made \n');
+        if (err) {
+            response.write('Unable to connect to the mongoDB server. Error:' + err + "\n");
+            //Error so close connection
+            db.close();
+        } else {
+            //HURRAY!! We are connected. :)
+            response.write('Connection established to' + url +"\n");
+
+            // do some work here with the database.
+
+            //Done Close connection
+            db.close();
+        }
+        response.end('Finished, Connection closed \n');
+    });
+
+}).listen(port);
+
