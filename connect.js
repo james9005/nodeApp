d73976046b300ae122fa807367b8d97694c1e1a1
James David Todd <james9005@hotmail.co.uk>
2015-11-30 14:44:26 +0000

    added stuff which works

--- connect.js
+++ connect.js
@@ -6,7 +6,7 @@ var mongodb = require('mongodb');
 var http = require('http');
 //setup our port
 var port = process.env.PORT || 1337;
-var url='mongodb://<James>:<playstation3>@ds054288.mongolab.com:54288/1201173';
+var url='mongodb://James:playstation3@ds054288.mongolab.com:54288/1201173';
 var MongoClient = mongodb.MongoClient;
 http.createServer(function(request, response) {
     response.writeHead(200, { 'Content-Type': 'text/plain' });
